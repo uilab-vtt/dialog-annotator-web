7e2bd863b507e6b9294ee21c4f734561af140e23
Jeongmin Byun <jmbyun91@gmail.com>
2019-02-13 15:20:20 +0900

    implement vue.js based color picker skeleton

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -6,9 +7,15 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World'
 
+def get_books():
+    with open('./data/book_data.json', 'r') as f:
+        books = json.load(f)
+    return books
+
 @app.route('/survey/<string:user_id>')
 def survey(user_id):
-    return user_id
+    books = get_books()
+    return render_template('survey.html', user_id=user_id, books=books)
 
 if __name__ == '__main__':
     app.run(debug=True)
